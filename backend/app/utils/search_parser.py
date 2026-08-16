6bf5ce5dd89dcfddd91c8725662f53299d03c9e1
Blomblo <63555404+mrblomblo@users.noreply.github.com>
2026-08-16 19:27:09 +0200

    Make uploaded at sort order an unconditional fallback

--- backend/app/utils/search_parser.py
+++ backend/app/utils/search_parser.py
@@ -515,8 +515,8 @@ def apply_search_criteria(query: Query, parsed_query: Dict[str, Any], db: Sessio
                          whens = {id_: i for i, id_ in enumerate(id_list)}
                          query = query.order_by(case(whens, value=Media.id))
                      except (ValueError, TypeError): pass
-    else:
-        if not query._order_by_clauses:
-             query = query.order_by(desc(Media.uploaded_at))
+
+    if not query._order_by_clauses:
+        query = query.order_by(desc(Media.uploaded_at))
 
     return query
